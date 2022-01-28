ea148a7da674c3a5d1ffa8d87d222e5d7ba33cee
Masanarea <ymoba626@icloud.com>
2022-01-28 12:41:58 +0900

    marge is a little difficult for me...

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,33 @@
-require('./bootstrap');
+import Vue from 'vue'
+
+
+var MyComponent = {
+    template:`
+    <div class="container">
+        <p>{{msg}}</p>
+        <hr>
+        <input v-on:keyup.enter="doAction" type="text" v-model="name">
+        <button v-on:click="doAction">click</button>
+    </div>`,
+    data: function(){
+        return{
+        msg:'please your name:',
+        name:'',
+        }
+    },
+    methods:{
+        doAction:function(){
+            this.msg = 'Hello, ' + this.name + '!!';
+    },
+    },
+}
+
+new Vue({
+    el: '#app',
+
+    components:{
+        'my-component':MyComponent
+},
+
+})
+
