7eb939d4ecf199e5c5b1bfa277e4599f6a0e0235
Masanarea <ymoba626@icloud.com>
2022-01-28 13:11:30 +0900

    display /,/login view(good)

---
+++ src/resources/js/App.vue
@@ -0,0 +1,12 @@
+// ルートコンポーネント
+// <div id="app"></div> にこのコンポーネントが描画
+<template>
+  <div>
+    <main>
+      <div class="container">
+<!-- <RouterView /> が切り替わる箇所を定義します。紙芝居の枠のようなもの -->
+        <RouterView />
+      </div>
+    </main>
+  </div>
+</template>
--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,33 +1,45 @@
+// import Vue from 'vue'
+
+
+// var MyComponent = {
+//     template:`
+//     <div class="container">
+//         <p>{{msg}}</p>
+//         <hr>
+//         <input v-on:keyup.enter="doAction" type="text" v-model="name">
+//         <button v-on:click="doAction">click</button>
+//     </div>`,
+//     data: function(){
+//         return{
+//         msg:'please your name:',
+//         name:'',
+//         }
+//     },
+//     methods:{
+//         doAction:function(){
+//             this.msg = 'Hello, ' + this.name + '!!';
+//     },
+//     },
+// }
+
+// new Vue({
+//     el: '#app',
+
+//     components:{
+//         'my-component':MyComponent
+// },
+
+// })
+
 import Vue from 'vue'
-
-
-var MyComponent = {
-    template:`
-    <div class="container">
-        <p>{{msg}}</p>
-        <hr>
-        <input v-on:keyup.enter="doAction" type="text" v-model="name">
-        <button v-on:click="doAction">click</button>
-    </div>`,
-    data: function(){
-        return{
-        msg:'please your name:',
-        name:'',
-        }
-    },
-    methods:{
-        doAction:function(){
-            this.msg = 'Hello, ' + this.name + '!!';
-    },
-    },
-}
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
 
 new Vue({
-    el: '#app',
-
-    components:{
-        'my-component':MyComponent
-},
-
+  el: '#app',
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
 })
-
