43185cbd379e00943884b910201c08890dcd4711
Masanarea <ymoba626@icloud.com>
2022-01-29 11:11:52 +0900

    I managed to resolve login&register error form ,It took a lot of concentration　for me

--- src/resources/js/pages/Login.vue
+++ src/resources/js/pages/Login.vue
@@ -14,29 +14,29 @@
     </ul>
     <div class="panel" v-show="tab === 1">
         <form class="form" @submit.prevent="login">
-        <label for="login-email">Email</label>
-        <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
-        <label for="login-password">Password</label>
-        <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
-        <div class="form__button">
-        <button type="submit" class="button button--inverse">login</button>
-        </div>
+            <label for="login-email">Email</label>
+            <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+            <label for="login-password">Password</label>
+            <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+            <div class="form__button">
+            <button type="submit" class="button button--inverse">login</button>
+            </div>
         </form>
     </div>
     <div class="panel" v-show="tab === 2">
         <div class="panel" v-show="tab === 2">
         <form class="form" @submit.prevent="register">
-        <label for="username">Name</label>
-        <input type="text" class="form__item" id="username" v-model="registerForm.name">
-        <label for="email">Email</label>
-        <input type="text" class="form__item" id="email" v-model="registerForm.email">
-        <label for="password">Password</label>
-        <input type="password" class="form__item" id="password" v-model="registerForm.password">
-        <label for="password-confirmation">Password (confirm)</label>
-        <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
-        <div class="form__button">
-        <button type="submit" class="button button--inverse">register</button>
-        </div>
+            <label for="username">Name</label>
+            <input type="text" class="form__item" id="username" v-model="registerForm.name">
+            <label for="email">Email</label>
+            <input type="text" class="form__item" id="email" v-model="registerForm.email">
+            <label for="password">Password</label>
+            <input type="password" class="form__item" id="password" v-model="registerForm.password">
+            <label for="password-confirmation">Password (confirm)</label>
+            <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+            <div class="form__button">
+            <button type="submit" class="button button--inverse">register</button>
+            </div>
         </form>
         </div>
     </div>
@@ -61,16 +61,20 @@ export default {
     }
   },
   methods: {
-    login () {
-      console.log(this.loginForm)
+    async login () {
+        // authストアのloginアクションを呼び出す
+        await this.$store.dispatch('auth/login', this.loginForm)
+
+        // トップページに移動する
+        this.$router.push('/')
     },
     async register () {
-    // authストアのresigterアクションを呼び出す
-    await this.$store.dispatch('auth/register', this.registerForm)
+        // authストアのresigterアクションを呼び出す
+        await this.$store.dispatch('auth/register', this.registerForm)
 
-    // トップページに移動する
-    this.$router.push('/')
-}
+        // トップページに移動する
+        this.$router.push('/')
+    }
   }
 }
 </script>
