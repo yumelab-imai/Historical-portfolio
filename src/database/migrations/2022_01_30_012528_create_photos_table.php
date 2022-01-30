84bc6856e6cf9d4995ee15c84c0d26d4dc80260a
Masanarea <ymoba626@icloud.com>
2022-01-30 14:13:08 +0900

    I managed to resolve a error when I run migration

---
+++ src/database/migrations/2022_01_30_012528_create_photos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            // ２つのメインkeyが重複
+            // $table->string('id');
+            // $table->bigInteger('user_id')->unsigned();
+            $table->string('id')->primary();
+            $table->unsignedInteger('user_id');
+            $table->string('filename');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
