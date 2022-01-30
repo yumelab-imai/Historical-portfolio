9e6c1c63bf16f9e9a257faa3cca216aae3254498
Masanarea <ymoba626@icloud.com>
2022-01-30 19:19:27 +0900

    send a test data to s3 on the AWS

--- src/resources/js/router.js
+++ src/resources/js/router.js
@@ -5,6 +5,7 @@ import VueRouter from 'vue-router'
 import PhotoList from './pages/PhotoList.vue'
 import Login from './pages/Login.vue'
 import SystemError from './pages/errors/System.vue'
+import PhotoDetail from './pages/PhotoDetail.vue'
 
 import store from './store'
 
@@ -17,6 +18,11 @@ const routes = [
   {
     path: '/',
     component: PhotoList
+  },
+  {
+    path: '/photos/:id',
+    component: PhotoDetail,
+    props: true
   },
   {
     path: '/login',
