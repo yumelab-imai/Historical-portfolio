ed8e325317d27fd1c2c85454e63d14ef564bba3f
Masanarea <ymoba626@icloud.com>
2022-02-05 09:30:46 +0900

    reconfirm login&register data-form view

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -22373,36 +22373,15 @@ createApp();
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _util__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./util */ "./resources/js/util.js");
 // ここでは Ajax 通信で用いる Axios ライブラリの設定を記述している
- // window._ = require('lodash');
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js"); // Ajaxリクエストであることを示すヘッダーを付与する
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
 window.axios.interceptors.request.use(function (config) {
   // クッキーからトークンを取り出してヘッダーに添付する
   config.headers['X-XSRF-TOKEN'] = Object(_util__WEBPACK_IMPORTED_MODULE_0__["getCookieValue"])('XSRF-TOKEN');
   return config;
-}); // import Echo from 'laravel-echo';
-// window.Pusher = require('pusher-js');
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-// axios の responseインターセプターにまとめる
+}); // axios の responseインターセプターにまとめる
 
 window.axios.interceptors.response.use(function (response) {
   return response;
--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -1,24 +1,12 @@
 // ここでは Ajax 通信で用いる Axios ライブラリの設定を記述している
 
 import { getCookieValue } from './util'
-// window._ = require('lodash');
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
 
 window.axios = require('axios');
 
 // Ajaxリクエストであることを示すヘッダーを付与する
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
 
 window.axios.interceptors.request.use(config => {
   // クッキーからトークンを取り出してヘッダーに添付する
@@ -26,18 +14,6 @@ window.axios.interceptors.request.use(config => {
 
   return config
 })
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-
 
 // axios の responseインターセプターにまとめる
 window.axios.interceptors.response.use(
