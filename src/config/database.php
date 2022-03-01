0e2a80f93bf40f143e1b1affbf4e70a9af971c98
Masanarea <ymoba626@icloud.com>
2022-03-01 14:51:08 +0900

    edit readm

--- src/config/database.php
+++ src/config/database.php
@@ -35,11 +35,6 @@ return [
 
     'connections' => [
 
-        'sqlite_testing' => [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
