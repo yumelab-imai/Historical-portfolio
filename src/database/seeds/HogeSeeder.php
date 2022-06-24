a367a1bb8389bdfc9bdb50ecb1f11fdda6402100
Masanarea <93495976+Masanarea@users.noreply.github.com>
2022-06-24 18:56:27 +0900

    viewComposerの開発の続き

--- src/database/seeds/HogeSeeder.php
+++ src/database/seeds/HogeSeeder.php
@@ -2,18 +2,18 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-// use Database\Seeders;
 
-class DatabaseSeeder extends Seeder
+class HogeSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(PhotoTableSeeder::class);
+        //
     }
 }
--- src/database/seeds/PhotoTableSeeder.php
+++ src/database/seeds/PhotoTableSeeder.php
@@ -5,6 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
+// 追加
+use Illuminate\Support\Str;
+use App\User;
+
+
 // 追加（20220623）
 use Illuminate\Support\Facades\DB;
 
@@ -19,22 +25,13 @@ class PhotoTableSeeder extends Seeder
     {
         // 追加（20220623）
         $param = [
-            'name' => 'taro',
-            'mail' => 'taro@yamada.jp',
-            'age' => 12,
+            // 'user_id' => fn () => \App\User::factory()->create()->id,
+            'id' => 1,
+            'user_id' => 4,
+            'filename' => Str::random(12) . '.jpg',
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
-        DB::table('people')->insert($param);
-        $param = [
-            'name' => 'hanako',
-            'mail' => 'hanako@flower.jp',
-            'age' => 34,
-        ];
-        DB::table('people')->insert($param);
-        $param = [
-            'name' => 'sachiko',
-            'mail' => 'sachiko@happy.jp',
-            'age' => 56,
-        ];
-        DB::table('people')->insert($param);
+        DB::table('photos')->insert($param);
     }
 }
